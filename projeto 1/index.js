716d4b11b09ca6e4e6390394b39ad83f75fe53f8
khadidjamoraes <khadidjamoraes@gmail.com>
2022-09-11 22:01:26 -0300

    Update index.js

--- projeto 1/index.js
+++ projeto 1/index.js
@@ -1,7 +1,7 @@
 
 const computerFactory = require("./computerFactory");
 
-const Pc1 = computerFactory(10, 227, 140, 'pc');      
-const Server1 = computerFactory(6, 144, 75,'server' );      
+const Pc1 = new computerFactory(10, 227, 140, 'pc');      
+const Server1 = new computerFactory(6, 144, 75,'server' );      
 console.log(Pc1.toString());      
 console.log(Server1.toString());
