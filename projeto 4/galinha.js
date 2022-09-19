2c8e369df47fd72a02b7ba0449257e19fd064eee
khadidjamoraes <khadidjamoraes@gmail.com>
2022-09-19 10:51:41 -0300

    Create galinha.js

---
+++ projeto 4/galinha.js
@@ -0,0 +1,12 @@
+
+// Criando a Classe Galinha 
+
+class Galinha {
+    constructor (){
+        this.cocorico  = () => 'Cocoricó';
+        this.voar  = () => 'Está voando';
+        return this 
+    }
+}
+
+module.exports = Galinha; 
